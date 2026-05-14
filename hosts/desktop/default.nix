42cb07e6f0066e0abc6aad0a39cf78f074cea509
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2026-05-14 13:34:10 +0200

    refactor(desktop): inline home-manager and podman modules
    Both modules were only used by the desktop host. Inline them into
    hosts/desktop/default.nix and remove the shared module files.

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -9,15 +9,14 @@
   modules.hyprland.profile = "desktop";
 
   imports = [
+    inputs.home-manager.nixosModules.default
     inputs.sops-nix.nixosModules.sops
     (modulesPath + "/installer/scan/not-detected.nix")
-    ../../modules/home-manager
     #../../modules/virtualization
     ../../modules/terminal
     ../../modules/neovim
     ../../modules/git
     ../../modules/hyprland
-    ../../modules/podman
   ];
 
   boot.loader = {
@@ -58,6 +57,16 @@
 
   security.polkit.enable = true;
 
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      autoPrune.enable = true;
+    };
+  };
+  users.users.lasse.extraGroups = [ "podman" ];
+
   environment.systemPackages = [
     pkgs.prusa-slicer
     pkgs.azuredatastudio
@@ -70,7 +79,21 @@
     pkgs.blender
     pkgs.sqlcmd
     pkgs.cura-appimage
+    pkgs.podman-compose
   ];
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.lasse = {
+      programs.home-manager.enable = true;
+      home = {
+        stateVersion = "23.05";
+        username = "lasse";
+        homeDirectory = "/home/lasse";
+      };
+    };
+  };
 
   zramSwap = {
     enable = true;
