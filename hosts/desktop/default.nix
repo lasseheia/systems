d0f44700ae8a1eb8c5cab34e62e0cac5918192bb
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2026-05-14 13:21:58 +0200

    refactor(desktop): inline shared nixos module
    Only the desktop host used modules/nixos, so inline its contents directly
    into hosts/desktop/default.nix and remove the shared module. This also
    fixes nixpkgs.stdenv.hostPlatform to the modern nixpkgs.hostPlatform.

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   inputs,
+  modulesPath,
   ...
 }:
 
@@ -9,7 +10,7 @@
 
   imports = [
     inputs.sops-nix.nixosModules.sops
-    ../../modules/nixos
+    (modulesPath + "/installer/scan/not-detected.nix")
     ../../modules/home-manager
     #../../modules/virtualization
     ../../modules/terminal
@@ -18,6 +19,44 @@
     ../../modules/hyprland
     ../../modules/podman
   ];
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    systemd-boot.configurationLimit = 10;
+  };
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+  nixpkgs.config.allowUnfree = true;
+
+  system.stateVersion = "23.05";
+  nix = {
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      auto-optimise-store = true;
+    };
+    optimise.automatic = true;
+  };
+
+  networking = {
+    useNetworkd = true;
+    wireless.iwd.enable = true;
+    interfaces.wlan0.useDHCP = true;
+    firewall.enable = true;
+  };
+
+  time.timeZone = "Europe/Oslo";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_TIME = "nb_NO.UTF-8";
+    };
+  };
+
+  security.polkit.enable = true;
 
   environment.systemPackages = [
     pkgs.prusa-slicer
